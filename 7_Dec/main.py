c650c137244ed4b239669ecbcf1f6c75709d17fc
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2023-12-07 08:30:24 +0100

    feat: add completion of part 2 of day 7

--- 7_Dec/main.py
+++ 7_Dec/main.py
@@ -14,13 +14,14 @@ from collections import defaultdict
 import functools
 
 card_types = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")
+card_types2 = "A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".split(", ")
 
 
-def compare_hands(hand1, hand2):
+def compare_hands(hand1, hand2, prioritization):
     for i in range(len(hand1)):
-        if card_types.index(hand1[i]) < card_types.index(hand2[i]):
+        if prioritization.index(hand1[i]) < prioritization.index(hand2[i]):
             return -1
-        elif card_types.index(hand1[i]) > card_types.index(hand2[i]):
+        elif prioritization.index(hand1[i]) > prioritization.index(hand2[i]):
             return 1
     return 0
 
@@ -61,7 +62,7 @@ def part1(file):
                 check.append(players[player_index][0])
             else:
                 sorted_hands = sorted(list(player_hand.get(i)),
-                                      key=functools.cmp_to_key(lambda x, y: compare_hands(x[1], y[1])))
+                                      key=functools.cmp_to_key(lambda x, y: compare_hands(x[1], y[1], card_types)))
                 for hand in sorted_hands:
                     rank = (len(players) - position)
                     player_bet = players[hand[0]][1]
@@ -73,9 +74,94 @@ def part1(file):
     print(score)
 
 def part2(file):
-    print("Qwer")
+    # Could use regex
 
+    combos = [[5], [1, 4], [2, 3], [1, 1, 3], [1, 2, 2], [1, 1, 1, 2], [1, 1, 1, 1, 1]]
+
+    hands = ["Five_Of_A_Kind", "Four_Of_A_Kind", "Full House", "Three_Of_A_Kind", "Two Pair", "One Pair", "High Card"]
+
+    players = []
+    for index, line in enumerate(file):
+        players.append(line.strip("\n").split(" "))
+
+    player_hand = defaultdict(list)
+    for i, player in enumerate(players):
+        card_combo = defaultdict(int)
+        hand = player[0]
+        for card in hand:
+            card_combo[card] += 1
+
+        # card_combo = list(card_combo.values())
+        # print(card_combo)
+        num_wildcards = card_combo['J']
+        card_combo.pop('J')
+        card_list = list(card_combo.values())
+        found = False
+        for index, combo in enumerate(combos):
+
+            if num_wildcards == 5:
+                player_hand[index].append([i, player[0]])
+                break
+            else:
+                card_list = sorted(card_list)
+                card_list[-1] += num_wildcards
+
+                for index, combo in enumerate(combos):
+                    if combo == card_list:
+                        player_hand[index].append([i, player[0]])
+                        found = True
+                        break
+
+            if found:
+                break
+            # combos = [[5], [1, 4], [2, 3], [1, 1, 3], [1, 2, 2], [1, 1, 1, 2], [1, 1, 1, 1, 1]]
+
+            #
+            # elif num_wildcards > 0 and len(combo) == len(card_list) - 1:
+            #     player_hand[index].append([i, player[0]])
+            #     print(card_combo.values())
+            #     break
+            # elif len(combo) == len(card_list):
+            #     print(card_combo.values())
+            #     player_hand[index].append([i, player[0]])
+            #     break
+
+
+
+    print(player_hand)
+    i = 0
+    score = 0
+    position = 0
+    check = []
+    while i < len(combos):
+        if player_hand.get(i) is not None:
+            if len(player_hand.get(i)) == 1:
+                player_index = player_hand.get(i)[0][0]
+                player_bet = players[player_index][1]
+                rank = (len(players) - position)
+                score += rank * int(player_bet)
+                position += 1
+                check.append(players[player_index][0])
+            else:
+                sorted_hands = sorted(list(player_hand.get(i)),
+                                      key=functools.cmp_to_key(lambda x, y: compare_hands(x[1], y[1], card_types2)))
+                print(sorted_hands)
+                for hand in sorted_hands:
+                    rank = (len(players) - position)
+                    player_bet = players[hand[0]][1]
+                    score += rank * int(player_bet)
+                    position += 1
+                    check.append(players[hand[0]][0])
+        i += 1
+
+    print(score)
+    print(check)
+    new_score = 0
+    for player in players:
+        new_score += (len(check) - check.index(player[0])) * int(player[1])
+
+    print(new_score)
 
 with open("input.txt", "r") as file:
-    part1(file)
-    # part2(file)
+    # part1(file)
+    part2(file)
