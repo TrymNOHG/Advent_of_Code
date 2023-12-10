6b1134ac7df9819a30d33f52db285bde429e4043
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2023-12-10 18:08:21 +0100

    feat: complete part 2 of day 10

--- 2023/10_Dec/main.py
+++ 2023/10_Dec/main.py
@@ -58,9 +58,6 @@ def part1(file):
 
     starting_moves = [left_move, down_move, right_move, up_move]
     for move in starting_moves:
-        # print(move)
-        # print(grid[move[1]])
-        # print(grid[move[1]][move[0]])
         is_valid_move, _, _ = is_move_valid(starting_pos, move, grid[move[1]][move[0]])
         if is_valid_move:
             valid_start_dir.append(move)  # Contains next index and the direction of pipe
@@ -105,15 +102,12 @@ def part2(file):
     valid_start_dir = []
 
     left_move = [(starting_pos[0] - 1, starting_pos[1]), 270]
-    down_move = [(starting_pos[0], starting_pos[1] + 1), 0]
+    down_move = [(starting_pos[0], starting_pos[1] + 1), 180]
     right_move = [(starting_pos[0] + 1, starting_pos[1]), 90]
-    up_move = [(starting_pos[0], starting_pos[1] - 1), 180]
+    up_move = [(starting_pos[0], starting_pos[1] - 1), 0]
 
     starting_moves = [left_move, down_move, right_move, up_move]
     for move in starting_moves:
-        # print(move)
-        # print(grid[move[1]])
-        # print(grid[move[1]][move[0]])
         is_valid_move, _, _ = is_move_valid(starting_pos, move[0], grid[move[0][1]][move[0][0]])
         if is_valid_move:
             valid_start_dir = move  # Contains next index and the direction of pipe
@@ -123,45 +117,23 @@ def part2(file):
     next_move = valid_start_dir[0]
     next_pipe = grid[valid_start_dir[0][1]][valid_start_dir[0][0]]
     rotation = valid_start_dir[1]
-    print(rotation)
     i = 0
 
-    # indexed_grid_map[next_move[1]][next_move[0]] = direction_map.get(rotation)
-    indexed_grid_map[next_move[1]][next_move[0]] = rotation
-
-
-    # Use just one angle again... Fix.
+    indexed_grid_map[next_move[1]][next_move[0]] = [rotation]
 
     while next_pipe != 'S':
         _, start_pipe_coords, new_rotation = is_move_valid(current, next_move, next_pipe)
         current = next_move
         next_move = start_pipe_coords
-        print(next_pipe)
-        print(new_rotation)
-        print(current)
+        old_rotation = rotation
         next_pipe = grid[next_move[1]][next_move[0]]
         rotation = (rotation + new_rotation) % 360
-        print(rotation)
-        # indexed_grid_map[current[1]][current[0]] = direction_map.get(rotation)
-        indexed_grid_map[current[1]][current[0]] = rotation
+        indexed_grid_map[current[1]][current[0]] = [old_rotation, rotation]
         if next_pipe == 'S':
-            # indexed_grid_map[next_move[1]][next_move[0]] = direction_map.get(valid_start_dir[1])
-            indexed_grid_map[next_move[1]][next_move[0]] = valid_start_dir[1]
+            indexed_grid_map[next_move[1]][next_move[0]] = [rotation, valid_start_dir[1]]
             break
 
-
-
-        # if next_pipe == 'S':
-        #     _, start_pipe_coords, new_rotation = is_move_valid(current, next_move, '|')
-        #     current = next_move
-        #     next_move = start_pipe_coords
-        #     next_pipe = grid[next_move[1]][next_move[0]]
-        #     rotation = (rotation + new_rotation) % 360
-        #     indexed_grid_map[next_move[1]][next_move[0]] = rotation
-        #     break
         i += 1
-
-    print(rotation)
 
     sum_of_non_pipe = 0
     for i, row in enumerate(indexed_grid_map):
@@ -171,18 +143,14 @@ def part2(file):
 
     area = 0
 
-    # Do I need to account for corner changes?
-
     for i, row in enumerate(indexed_grid_map):
-        previous_num = -1
+        previous_num = [-1]
         for j, entry in enumerate(row):
             if entry == '.':
                 is_in = False
                 # Check left first
-                if j > 0 and previous_num == 0:
+                if j > 0 and 0 in previous_num:
                     is_in = True
-
-                # if previous_num == [-1]:
 
                 # Find right pipe
                 if not is_in and j < len(row) - 1:
@@ -192,7 +160,7 @@ def part2(file):
                         index += 1
                         right_pip = row[j + 1 + index]
 
-                    if type(right_pip) == list and right_pip == 180:
+                    if type(right_pip) == list and 180 in right_pip == 180:
                         is_in = True
 
                 # Check above
@@ -205,7 +173,6 @@ def part2(file):
                     if above_pipe == 270:
                         is_in = True
 
-
                 # Check below pipe
                 if not is_in and i < len(indexed_grid_map) - 1:
                     below_pipe = indexed_grid_map[i + 1][j]
@@ -215,8 +182,7 @@ def part2(file):
                         below_pipe = indexed_grid_map[i + 1 + index][j]
                     if below_pipe == 90:
                         is_in = True
-                # else:
-                #     continue
+
                 if is_in:
                     area += 1
                     indexed_grid_map[i][j] = 'I'
@@ -224,78 +190,11 @@ def part2(file):
                 previous_num = entry
 
     for row in indexed_grid_map:
-        # print(''.join(row))
         print(row)
 
     print(area)
-    print(sum_of_non_pipe)
 
-def part3(file):
-    grid = [[entry for entry in line.strip("\n")] for line in file]
-    starting_pos = ()
-    for i, row in enumerate(grid):
-        for j, entry in enumerate(row):
-            if entry == 'S':
-                starting_pos = (j, i)
-
-    indexed_grid_map = [[10000000 for _ in row] for row in grid]
-
-    valid_start_dir = []
-
-    left_move = (starting_pos[0] - 1, starting_pos[1])
-    down_move = (starting_pos[0], starting_pos[1] + 1)
-    right_move = (starting_pos[0] + 1, starting_pos[1])
-    up_move = (starting_pos[0], starting_pos[1] - 1)
-
-    starting_moves = [left_move, down_move, right_move, up_move]
-    for move in starting_moves:
-        # print(move)
-        # print(grid[move[1]])
-        # print(grid[move[1]][move[0]])
-        is_valid_move, _, _ = is_move_valid(starting_pos, move, grid[move[1]][move[0]])
-        if is_valid_move:
-            valid_start_dir.append(move)  # Contains next index and the direction of pipe
-
-    valid_start_dir = [valid_start_dir[0]]
-    for start_pipe_coords in valid_start_dir:
-        current = starting_pos
-        next_move = start_pipe_coords
-        next_pipe = grid[start_pipe_coords[1]][start_pipe_coords[0]]
-        i = 0
-        while next_pipe != 'S':
-            _, start_pipe_coords, _ = is_move_valid(current, next_move, next_pipe)
-            prev = current
-            current = next_move
-            next_move = start_pipe_coords
-            next_pipe = grid[next_move[1]][next_move[0]]
-
-            indexed_grid_map[prev[1]][prev[0]] = min(indexed_grid_map[prev[1]][prev[0]], i)
-
-            if next_pipe == 'S':
-                _, start_pipe_coords, _ = is_move_valid(current, next_move, '|')
-                prev = current
-                current = next_move
-                next_move = start_pipe_coords
-                next_pipe = grid[next_move[1]][next_move[0]]
-
-                indexed_grid_map[prev[1]][prev[0]] = min(indexed_grid_map[prev[1]][prev[0]], i)
-                break
-            i += 1
-
-    max_val = 0
-    for i, row in enumerate(indexed_grid_map):
-        print(row)
-        for j, entry in enumerate(row):
-            if not entry == 10000000:
-                max_val = max(max_val, entry)
-
-    print(max_val)
 
 with open("input.txt", "r") as file:
     # part1(file)
-    # part3(file)
     part2(file)
-
-
-
-# Fix that last point is not included in index!!!
